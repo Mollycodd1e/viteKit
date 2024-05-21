c778339dd87d2b698b4d463ff5c2a0489b940271
iivanov <selivanov@realx.tech>
2024-05-21 18:51:54 +0300

    add import css to bundle js

--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,10 @@ import { resolve } from 'path'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import dts from 'vite-plugin-dts'
+import { libInjectCss } from 'vite-plugin-lib-inject-css'
 
 export default defineConfig({
-	plugins: [
-		react(),
-		dts({ include: ['lib'] })
-	],
+	plugins: [react(), libInjectCss(), dts({ include: ['lib'] })],
 	build: {
 		lib: {
 			entry: resolve(__dirname, 'lib/main.ts'),
@@ -15,7 +13,7 @@ export default defineConfig({
 		},
 		rollupOptions: {
 			external: ['react', 'react/jsx-runtime'],
-		}
+		},
 	},
 })
 
