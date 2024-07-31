bc16122fd41a21c5b293e3f625a04bfeae762f21
Селиванов Иван <iselivanov@realx.local>
2024-07-31 12:52:58 +0300

    Release 0.0.111

--- scripts/commitAndPush.js
+++ scripts/commitAndPush.js
@@ -1,10 +1,19 @@
-import { exec } from 'child_process';
+import { exec } from 'child_process'
+import { readFile } from 'fs/promises'
 
-exec('git add . && git commit -m "Release ${npm_package_version}" && git push origin master && npm publish', (error, stdout, stderr) => {
-  if (error) {
-    console.error(`Error executing command: ${error}`);
-    return;
-  }
-  console.log(`stdout: ${stdout}`);
-  console.error(`stderr: ${stderr}`);
-});
+const packagePath = './package.json'
+const packageJson = JSON.parse(await readFile(packagePath, 'utf-8'))
+
+const versionParts = packageJson.version
+exec(
+	`git add . && git commit -m "Release ${versionParts}" && git push origin master && npm publish`,
+	(error, stdout, stderr) => {
+		if (error) {
+			console.error(`Error executing command: ${error}`)
+			return
+		}
+		console.log(`stdout: ${stdout}`)
+		console.error(`stderr: ${stderr}`)
+	}
+)
+
