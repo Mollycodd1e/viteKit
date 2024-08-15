52a0be9a94c381f847e40b4f3bddf4235925436b
Селиванов Иван <iselivanov@realx.local>
2024-08-15 17:20:31 +0300

    Release 0.0.181

--- src/components/SliderNavigation/ui/SliderNavigation.tsx
+++ src/components/SliderNavigation/ui/SliderNavigation.tsx
@@ -1,47 +1,50 @@
-import {RoundButton} from "../../RoundButton";
-import {Tag} from "../../Tag";
-import {useClientWidth} from "../../../shared/useClientWidth.ts";
-import {ISliderNavigation} from "./SliderNavigation.types.ts";
-
-
+import { RoundButton } from '../../RoundButton'
+import { Tag } from '../../Tag'
+import { useClientWidth } from '../../../shared/useClientWidth.ts'
+import { ISliderNavigation } from './SliderNavigation.types.ts'
+import classNames from 'classnames'
+import s from './SliderNavigation.module.scss'
+const cx = classNames.bind(s)
 export const SliderNavigation = ({
-                                     indexSlide,
-                                     goPrev,
-                                     goNext,
-                                     arr,
-                                     isNeedTag = true,
-                                     additionalClassTag = '',
-                                     navigationClassName = '',
-                                     additionalClassNavButton = ''
-                                 }: ISliderNavigation) => {
-    const { isMobile} = useClientWidth()
+	indexSlide,
+	goPrev,
+	goNext,
+	arr,
+	isNeedTag = true,
+	additionalClassTag = '',
+	navigationClassName = '',
+	additionalClassNavButton = '',
+}: ISliderNavigation) => {
+	const { isMobile } = useClientWidth()
 
-    return <div className={navigationClassName}>
-        <RoundButton
-            size={isMobile ? 'medium' : 'large'}
-            iconName="directionDown"
-            deg="90"
-            disabled={indexSlide === 0}
-            additionalClass={additionalClassNavButton}
-            onClick={goPrev}
-        />
+	return (
+		<div className={navigationClassName}>
+			<RoundButton
+				size={isMobile ? 'medium' : 'large'}
+				iconName='directionDown'
+				deg='90'
+				disabled={indexSlide === 0}
+				additionalClass={additionalClassNavButton}
+				onClick={goPrev}
+			/>
 
-        {isNeedTag && <Tag
-            style={{backgroundColor: '#141416'}}
-            additionalClass={additionalClassTag}
-            variant="shade"
-            size={isMobile ? 'medium' : 'large'}>
-            {`${indexSlide + 1} из ${arr.length}`}
-        </Tag>}
+			{isNeedTag && (
+				<Tag
+					additionalClass={cx(s.sliderTag, additionalClassTag)}
+					variant='shade'
+					size={isMobile ? 'medium' : 'large'}>
+					{`${indexSlide + 1} из ${arr.length}`}
+				</Tag>
+			)}
 
-        <RoundButton
-            disabled={indexSlide === arr.length - 1}
-            size={isMobile ? 'medium' : 'large'}
-            iconName="directionDown"
-            deg="-90"
-            additionalClass={additionalClassNavButton}
-            onClick={goNext}
-        />
-    </div>
+			<RoundButton
+				disabled={indexSlide === arr.length - 1}
+				size={isMobile ? 'medium' : 'large'}
+				iconName='directionDown'
+				deg='-90'
+				additionalClass={additionalClassNavButton}
+				onClick={goNext}
+			/>
+		</div>
+	)
 }
-
