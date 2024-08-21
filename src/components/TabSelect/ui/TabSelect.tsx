fd266bd325b3247ecc1e9986ea88ecd4f803952e
ktiukalov <tiukalov@realx.tech>
2024-08-21 18:36:34 +0300

    Release 0.0.201

--- src/components/TabSelect/ui/TabSelect.tsx
+++ src/components/TabSelect/ui/TabSelect.tsx
@@ -1,5 +1,5 @@
 'use client'
-import {Children, cloneElement, forwardRef} from 'react'
+import {forwardRef} from 'react'
 import s from './TabSelect.module.scss'
 import classNames from 'classnames'
 import {Text} from '../../Text'
@@ -20,7 +20,7 @@ type TBtn = {
 type BaseInputProps = JSX.IntrinsicElements['input'] & TBtn
 
 interface ITabSelect {
-    children?: React.ReactElement<HTMLButtonElement> | React.ReactElement<HTMLButtonElement>[]
+    children?: React.ReactNode | React.ReactNode[]
     variant?: SwitcherVariant
     classname?: string
     isRound?: boolean
@@ -34,16 +34,9 @@ export const TAB_SELECT_VARIANTS = {
 export type SwitcherVariant = keyof typeof TAB_SELECT_VARIANTS
 
 const TabSelect = ({variant = 'whiteFill', classname, children}: ITabSelect) => {
-    if (!children || !Array.isArray(children)) return null
-
-    //TODO предлогаю засунуть в жопу Children, cloneElement т.к это легаси хуйня
     return (
         <div className={cx(s.root, s[`${variant}`], classname)}>
-            {Children.map(children, (child) => {
-                return cloneElement(child, {
-                    className: cx(s.item),
-                })
-            })}
+            {children}
         </div>
     )
 }
@@ -74,7 +67,7 @@ const TabSelectItem = forwardRef<HTMLInputElement, BaseInputProps>((props, ref) 
     //isLong атрибут убирает ограничения в width, height 48px
 
     return (
-        <label className={cx(additionalLabelClass, {[s.withPadding]: withPadding})}>
+        <label className={cx(s.item, additionalLabelClass, {[s.withPadding]: withPadding})}>
             <input ref={ref} type='checkbox' checked={checked} {...rest}/>
             <div className={cx(className, additionalClassName, {isLong})}
                  style={{width: getWidth(isTablet, isDesktop)}}>
