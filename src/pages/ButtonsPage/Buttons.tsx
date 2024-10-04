06c14ea1cf5f8524a12624f48ef986117621f90d
Селиванов Иван <iselivanov@realx.local>
2024-10-04 12:50:40 +0300

    Release 0.0.260

--- src/pages/ButtonsPage/Buttons.tsx
+++ src/pages/ButtonsPage/Buttons.tsx
@@ -6,7 +6,7 @@ import { ZoomControls } from '../../components/ZoomControls'
 
 export const ButtonsPage = () => {
 	const [isActive, setIsActive] = useState<boolean>(false)
-
+	const [isActive1, setIsActive1] = useState<boolean>(false)
 	return (
 		<div className={s.root}>
 			<div className={s.clearButton}>
@@ -54,7 +54,16 @@ export const ButtonsPage = () => {
 					goPrev={() => {}}
 				/>
 			</div>
-			<Switcher isActive={isActive} onClick={() => setIsActive(!isActive)}>В продаже</Switcher>
+			<Switcher
+				isActive={isActive}
+				onClick={() => setIsActive(!isActive)}>
+				В продаже
+			</Switcher>
+			<Switcher
+				isActive={isActive1}
+				onClick={() => setIsActive1(!isActive1)}>
+				В продаже
+			</Switcher>
 		</div>
 	)
 }
