7bf694d682e3e604bc925d1733dca7aba9b57b99
Mollycodd1e <selik7@mail.ru>
2024-10-20 03:47:33 +0300

    Release version 0.0.303

--- declaration.d.ts
+++
@@ -1,9 +0,0 @@
-declare module '*.svg' {
-	const content: string
-	export default content
-}
-
-declare module '*.module.scss' {
-	const classes: { [key: string]: string }
-	export default classes
-}
