b10c62acc38f68c950660ecbf302148bbc86b991
Mollycodd1e <selik7@mail.ru>
2024-10-20 04:48:16 +0300

    $npm_config_commit_message

--- scripts/commitAndPush.js
+++ scripts/commitAndPush.js
@@ -3,14 +3,15 @@ import { readFile } from 'fs/promises'
 import readline from 'readline'
 
 async function getCommitMessage() {
+	const rl = readline.createInterface({
+		input: process.stdin,
+		output: process.stdout,
+	})
+
 	return new Promise((resolve) => {
-		const rl = readline.createInterface({
-			input: process.stdin,
-			output: process.stdout,
-		})
-		rl.question('Введите сообщение коммита: ', (answer) => {
+		rl.question('Введите сообщение коммита: ', (message) => {
 			rl.close()
-			resolve(answer)
+			resolve(message)
 		})
 	})
 }
@@ -20,11 +21,8 @@ async function commitAndPush() {
 		const packagePath = './package.json'
 		const packageJson = JSON.parse(await readFile(packagePath, 'utf-8'))
 		const versionParts = packageJson.version
-		let commitMessage = process.argv.slice(2).join(' ')
-
-		if (!commitMessage) {
-			commitMessage = (await getCommitMessage()) + ` (${versionParts})`
-		}
+		const commitMessage =
+			process.argv[2] || (await getCommitMessage()) || `Release version ${versionParts}`
 
 		exec(
 			`git add . && git commit -m "${commitMessage}" && git push origin master`,
