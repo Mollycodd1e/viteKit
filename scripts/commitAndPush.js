d2de148b0e8467ec377b5c7c224a7b100899e90a
Mollycodd1e <selik7@mail.ru>
2024-10-20 04:55:06 +0300

    $npm_config_m

--- scripts/commitAndPush.js
+++ scripts/commitAndPush.js
@@ -1,31 +1,15 @@
 import { exec } from 'child_process'
 import { readFile } from 'fs/promises'
-import readline from 'readline'
-
-async function getCommitMessage() {
-	const rl = readline.createInterface({
-		input: process.stdin,
-		output: process.stdout,
-	})
-
-	return new Promise((resolve) => {
-		rl.question('Введите сообщение коммита: ', (message) => {
-			rl.close()
-			resolve(message)
-		})
-	})
-}
 
 async function commitAndPush() {
 	try {
 		const packagePath = './package.json'
 		const packageJson = JSON.parse(await readFile(packagePath, 'utf-8'))
 		const versionParts = packageJson.version
-		const commitMessage =
-			process.argv[2] || (await getCommitMessage()) || `Release version ${versionParts}`
-		const sanitizedCommitMessage = commitMessage.replace(/"/g, '\\"').replace(/'/g, "\\'").trim()
+		const commitMessage = process.argv[2] || `Release version ${versionParts}`
+
 		exec(
-			`git add . && git commit -m "${sanitizedCommitMessage}" && git push origin master`,
+			`git add . && git commit -m "${commitMessage}" && git push origin master`,
 			(error, stdout, stderr) => {
 				if (error) {
 					console.error(`Error during git operations: ${error}`)
