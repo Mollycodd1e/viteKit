891593c759c3d38d9f13d90fd0c2504d50f23a36
Mollycodd1e <selik7@mail.ru>
2024-10-20 04:37:44 +0300

    testik (0.0.319)

--- scripts/commitAndPush.js
+++ scripts/commitAndPush.js
@@ -1,12 +1,30 @@
 import { exec } from 'child_process'
 import { readFile } from 'fs/promises'
+import readline from 'readline'
+
+async function getCommitMessage() {
+	return new Promise((resolve) => {
+		const rl = readline.createInterface({
+			input: process.stdin,
+			output: process.stdout,
+		})
+		rl.question('Введите сообщение коммита: ', (answer) => {
+			rl.close()
+			resolve(answer)
+		})
+	})
+}
 
 async function commitAndPush() {
 	try {
 		const packagePath = './package.json'
 		const packageJson = JSON.parse(await readFile(packagePath, 'utf-8'))
 		const versionParts = packageJson.version
-		const commitMessage = process.argv.slice(2).join(' ') || `Release version ${versionParts}`
+		let commitMessage = process.argv[2]
+
+		if (!commitMessage) {
+			commitMessage = (await getCommitMessage()) + ` (${versionParts})`
+		}
 
 		exec(
 			`git add . && git commit -m "${commitMessage}" && git push origin master`,
