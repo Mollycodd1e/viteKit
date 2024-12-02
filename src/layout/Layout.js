36d8c69d0fbb03c7c4e4e7fc28201ecb68669654
Mollycodd1e <selik7@mail.ru>
2024-12-03 00:17:47 +0300

    Update version to 0.0.396

--- src/layout/Layout.js
+++
@@ -1,7 +0,0 @@
-import { jsx as _jsx, jsxs as _jsxs } from "react/jsx-runtime";
-import { Header } from './components/Header/Header.tsx';
-import s from './style.module.scss';
-const Layout = ({ children }) => {
-    return (_jsxs("div", { className: s.layout, children: [_jsx(Header, {}), _jsx("main", { children: children })] }));
-};
-export default Layout;
