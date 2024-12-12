6814bfc83e9dcc0bee9dd42437944f006c414744
Селиванов Иван <iselivanov@realx.local>
2024-12-12 15:08:00 +0300

    Update version to 0.0.417

--- src/components/FormModal/ui/ModalForm.tsx
+++ src/components/FormModal/ui/ModalForm.tsx
@@ -1,220 +1,262 @@
 import InputMask from 'react-input-mask'
 import s from './ModalForm.module.scss'
-import {Modal} from '../../Modal'
-import {Flex} from '../../Flex'
-import {Input} from '../../Input'
-import {useForm} from 'react-hook-form'
-import {Button} from '../../Button'
-import {NewIcon} from '../../NewIcon'
-import {emailReg, nameReg, phoneReg} from '../utils/reg.ts'
-import {CheckBox} from '../../CheckBox/CheckBox.tsx'
-import {TextArea} from '../../TextArea/TextArea.tsx'
-import {useClientWidth} from '../../../../lib/shared/useClientWidth.ts'
+import { Modal } from '../../Modal'
+import { Flex } from '../../Flex'
+import { Input } from '../../Input'
+import { useForm } from 'react-hook-form'
+import { Button } from '../../Button'
+import { NewIcon } from '../../NewIcon'
+import { emailReg, nameReg, phoneReg } from '../utils/reg.ts'
+import { CheckBox } from '../../CheckBox/CheckBox.tsx'
+import { TextArea } from '../../TextArea/TextArea.tsx'
+import { useClientWidth } from '../../../../lib/shared/useClientWidth.ts'
 
 interface IModalFormProps {
-    isFormOpen: boolean
-    setIsFormOpen: (isOpen: boolean) => void
-    submitHandler: (data: IFormPageInputs) => void
-    modalWidth?: string
-    isPhone?: boolean
-    title: string
-    isEmail?: boolean
-    isTextArea?: boolean
-    subTitle: string
-    advCheckBox?: { text: string; isRequired: boolean } | undefined | null
-    textAreaPlaceholder?: string
-    rowsTextArea?: number
-    isRequiredPhone?: boolean
-    isRequiredEmail?: boolean
-    isRequiredPhoneOrEmail?: boolean
+	isFormOpen: boolean
+	setIsFormOpen: (isOpen: boolean) => void
+	submitHandler: (data: IFormPageInputs) => void
+	modalWidth?: string
+	isPhone?: boolean
+	title: string
+	isEmail?: boolean
+	telegramLink?: string
+	isTextArea?: boolean
+	subTitle: string
+	advCheckBox?: { text: string; isRequired: boolean } | undefined | null
+	textAreaPlaceholder?: string
+	rowsTextArea?: number
+	isRequiredPhone?: boolean
+	isNews?: boolean
+	isRequiredEmail?: boolean
+	isRequiredPhoneOrEmail?: boolean
 }
 
 type IFormPageInputs = {
-    phone: string
-    name: string
-    email?: string
-    personalCheckBox: boolean
-    advCheckBox?: boolean
-    textarea?: string
+	phone: string
+	name: string
+	email?: string
+	personalCheckBox: boolean
+	advCheckBox?: boolean
+	textarea?: string
 }
 
 const defaultTextCheckBox =
-    'Я принимаю условия Политики обработки и защиты персональных данных, даю согласия на обработку персональных данных'
+	'Я принимаю условия Политики обработки и защиты персональных данных, даю согласия на обработку персональных данных'
 
 export const ModalForm = ({
-                              isFormOpen,
-                              setIsFormOpen,
+	isFormOpen,
+	setIsFormOpen,
+	isNews,
+	title,
+	isEmail,
+	isPhone,
+	isTextArea,
+	telegramLink = 'https://t.me/stone_developer',
+	isRequiredPhone = true,
+	isRequiredEmail = false,
+	isRequiredPhoneOrEmail,
+	textAreaPlaceholder,
+	subTitle,
+	submitHandler,
+	modalWidth = '442px',
+	rowsTextArea = 2,
+	advCheckBox = { text: defaultTextCheckBox, isRequired: true },
+}: IModalFormProps) => {
+	const {
+		register,
+		handleSubmit,
+		trigger,
+		reset,
+		watch,
+		formState: { errors },
+	} = useForm<IFormPageInputs>()
 
-                              title,
-                              isEmail,
-                              isPhone,
-                              isTextArea,
-                              isRequiredPhone = true,
-                              isRequiredEmail = false,
-                              isRequiredPhoneOrEmail,
-                              textAreaPlaceholder,
-                              subTitle,
-                              submitHandler,
-                              modalWidth = '442px',
-                              rowsTextArea = 2,
-                              advCheckBox = {text: defaultTextCheckBox, isRequired: true},
-                          }: IModalFormProps) => {
-    const {
-        register,
-        handleSubmit,
-        trigger,
-        reset,
-        watch,
-        formState: {errors},
-    } = useForm<IFormPageInputs>()
+	const isEmailFill = Boolean(watch('email'))
 
-    const isEmailFill = Boolean(watch('email'))
+	const { onChange, onBlur, ...rest } = register('phone', {
+		required: isRequiredPhone || isRequiredPhoneOrEmail ? !isEmailFill : true,
+		pattern: { value: phoneReg, message: '' },
+		onBlur: () => trigger('phone'),
+	})
 
-    const {onChange, onBlur, ...rest} = register('phone', {
-        required: isRequiredPhone || isRequiredPhoneOrEmail ? !isEmailFill : true,
-        pattern: {value: phoneReg, message: ''},
-        onBlur: () => trigger('phone'),
-    })
+	const { isMobile, currentClientWidth } = useClientWidth()
 
-    const {isMobile, currentClientWidth} = useClientWidth()
+	const isCheckedPersonal = watch('personalCheckBox')
+	const isCheckAdvCheckBox = watch('advCheckBox')
 
-    const isCheckedPersonal = watch('personalCheckBox')
-    const isCheckAdvCheckBox = watch('advCheckBox')
+	const onSubmit = (data: IFormPageInputs) => {
+		submitHandler(data)
+		reset()
+	}
 
-    const onSubmit = (data: IFormPageInputs) => {
-        submitHandler(data)
-        reset()
-    }
+	if (!currentClientWidth) return null
 
-    if (!currentClientWidth) return null
+	return (
+		<Modal
+			isOpen={isFormOpen}
+			emitIsOpen={() => setIsFormOpen(false)}
+			additionalClass={s.modal}
+			opacity={0.7}
+			isTransparentBack={true}
+			additionalClassModalBody={s.modalBody}>
+			<div
+				className={s.root}
+				style={{ width: isMobile ? '100%' : modalWidth }}>
+				<div
+					className={s.title}
+					dangerouslySetInnerHTML={{ __html: title ?? 'Оставить обращение' }}
+				/>
+				{isNews && (
+					<div className={s.telegramWrapper}>
+						<a
+							className={s.telegramLink}
+							href={telegramLink}
+							target='_blank'>
+							<div className={s.telegram}>
+								<div>Подписаться в Telegram</div>
+								<NewIcon name='telegram'></NewIcon>
+							</div>
+						</a>
+						<div className={s.orText}>или</div>
+					</div>
+				)}
+				{!isNews && (
+					<div
+						className={s.description}
+						dangerouslySetInnerHTML={{
+							__html:
+								subTitle && !isNews
+									? `Оставьте номер телефона для получения консультации`
+									: `${(<div>привет</div>)}`,
+						}}
+					/>
+				)}
+				<form onSubmit={handleSubmit(onSubmit)}>
+					<Flex className={s.inputWrapper}>
+						<Input
+							width='full'
+							size_s='small'
+							variant='light'
+							placeholder='Введите имя'
+							data-testid='modal_name'
+							{...register('name', { required: true, pattern: nameReg })}
+							error={Boolean(errors.name)}
+						/>
 
-    return (
-        <Modal
-            isOpen={isFormOpen}
-            emitIsOpen={() => setIsFormOpen(false)}
-            additionalClass={s.modal}
-            opacity={0.7}
-            isTransparentBack={true}
-            additionalClassModalBody={s.modalBody}>
-            <div
-                className={s.root}
-                style={{width: isMobile ? '100%' : modalWidth}}>
-                <div
-                    className={s.title}
-                    dangerouslySetInnerHTML={{__html: title ?? 'Оставить обращение'}}
-                />
-                <div
-                    className={s.description}
-                    dangerouslySetInnerHTML={{
-                        __html: subTitle ?? `Оставьте номер телефона для получения консультации`,
-                    }}
-                />
-                <form onSubmit={handleSubmit(onSubmit)}>
-                    <Flex className={s.inputWrapper}>
-                        <Input
-                            width='full'
-                            size_s='small'
-                            variant='light'
-                            placeholder='Введите имя'
-                            data-testid='modal_name'
-                            {...register('name', {required: true, pattern: nameReg})}
-                            error={Boolean(errors.name)}
-                        />
+						{isPhone && (
+							<InputMask
+								inputMode='numeric'
+								name='phone'
+								mask={'+7 (999) 999-99-99'}
+								onChange={onChange}
+								placeholder='+7 (- - -)&ensp;- - -&ensp;- -&ensp;- -'
+								onBlur={onBlur}>
+								<Input
+									width='full'
+									data-testid='phone_modal'
+									type='tel'
+									size_s={'small'}
+									variant='light'
+									{...rest}
+									{...register('phone')}
+									error={Boolean(errors.phone)}
+								/>
+							</InputMask>
+						)}
 
-                        {isPhone && (
-                            <InputMask
-                                inputMode='numeric'
-                                name='phone'
-                                mask={'+7 (999) 999-99-99'}
-                                onChange={onChange}
-                                placeholder='+7 (- - -)&ensp;- - -&ensp;- -&ensp;- -'
-                                onBlur={onBlur}>
-                                <Input
-                                    width='full'
-                                    data-testid='phone_modal'
-                                    type='tel'
-                                    size_s={'small'}
-                                    variant='light'
-                                    {...rest}
-                                    {...register('phone')}
-                                    error={Boolean(errors.phone)}
-                                />
-                            </InputMask>
-                        )}
+						{isEmail && (
+							<Input
+								width='full'
+								size_s='small'
+								variant='light'
+								placeholder='Введите email'
+								data-testid='modal_email'
+								{...register('email', {
+									required: isRequiredEmail || isRequiredPhoneOrEmail ? isEmailFill : false,
+									pattern: emailReg,
+								})}
+								error={Boolean(errors.email)}
+							/>
+						)}
 
-                        {isEmail && (
-                            <Input
-                                width='full'
-                                size_s='small'
-                                variant='light'
-                                placeholder='Введите email'
-                                data-testid='modal_email'
-                                {...register('email', {
-                                    required: isRequiredEmail || isRequiredPhoneOrEmail ? isEmailFill : false,
-                                    pattern: emailReg,
-                                })}
-                                error={Boolean(errors.email)}
-                            />
-                        )}
+						{isTextArea && (
+							<TextArea
+								data-testid='modal_textarea'
+								placeholder={textAreaPlaceholder}
+								rows={rowsTextArea}
+								width={'full'}
+								{...register('textarea')}
+							/>
+						)}
 
-                        {isTextArea && (
-                            <TextArea
-                                data-testid='modal_textarea'
-                                placeholder={textAreaPlaceholder}
-                                rows={rowsTextArea}
-                                width={'full'}
-                                {...register('textarea')}
-                            />
-                        )}
+						{advCheckBox && !isNews && (
+							<CheckBox
+								data-testid='modal_advCheckBox'
+								isChecked={isCheckAdvCheckBox}
+								error={Boolean(errors.advCheckBox)}
+								text={advCheckBox.text}
+								{...register('advCheckBox', { required: advCheckBox?.isRequired })}
+							/>
+						)}
 
-                        {advCheckBox && (
-                            <CheckBox
-                                data-testid='modal_advCheckBox'
-                                isChecked={isCheckAdvCheckBox}
-                                error={Boolean(errors.advCheckBox)}
-                                text={advCheckBox.text}
-                                {...register('advCheckBox', {required: advCheckBox?.isRequired})}
-                            />
-                        )}
+						<CheckBox
+							data-testid='modal_personalCheckBox'
+							isChecked={isCheckedPersonal}
+							error={Boolean(errors.personalCheckBox)}
+							{...register('personalCheckBox', { required: isNews ? false : true })}
+							//можно указывать либо text либо children
+						>
+							{isNews && (
+								<div>
+									Согласен(-на){' '}
+									<a
+										className={s.linkPolicy}
+										href={'/documents/soglasie-na-rassylki'}>
+										получать новости, информацию по сделке и рекламу
+									</a>{' '}
+									от Stone
+								</div>
+							)}
+							{!isNews && (
+								<div>
+									Я принимаю условия{' '}
+									<a
+										className={s.linkPolicy}
+										href={'/documents/docs'}>
+										Политики обработки и защиты персональных данных
+									</a>
+									, даю согласия на обработку персональных данных
+								</div>
+							)}
+						</CheckBox>
 
-                        <CheckBox
-                            data-testid='modal_personalCheckBox'
-                            isChecked={isCheckedPersonal}
-                            error={Boolean(errors.personalCheckBox)}
-                            {...register('personalCheckBox', {required: true})}
-                            //можно указывать либо text либо children
-                        >
-                            <div>Я принимаю условия <a className={s.linkPolicy} href={'/documents/docs'}>Политики обработки и защиты персональных данных</a>, даю согласия на
-                                обработку персональных данных</div>
-                        </CheckBox>
+						<Button
+							data-testid='modal_submit'
+							variant='blue'
+							width='full'
+							type='submit'
+							size='large'
+							as='button'
+							additionalClass={s.modalBtn}>
+							Отправить
+						</Button>
+					</Flex>
 
-                        <Button
-                            data-testid='modal_submit'
-                            variant='blue'
-                            width='full'
-                            type='submit'
-                            size='large'
-                            as='button'
-                            additionalClass={s.modalBtn}>
-                            Отправить
-                        </Button>
-                    </Flex>
-
-                    <Button
-                        additionalClass={s.btn}
-                        onClick={() => setIsFormOpen(false)}
-                        as='button'
-                        variant='gray'
-                        type={'button'}
-                        size='small'>
-                        <NewIcon
-                            name='close'
-                            size='16'
-                            color='#141416'
-                        />
-                    </Button>
-                </form>
-            </div>
-        </Modal>
-    )
+					<Button
+						additionalClass={s.btn}
+						onClick={() => setIsFormOpen(false)}
+						as='button'
+						variant='gray'
+						type={'button'}
+						size='small'>
+						<NewIcon
+							name='close'
+							size='16'
+							color='#141416'
+						/>
+					</Button>
+				</form>
+			</div>
+		</Modal>
+	)
 }
--- src/pages/FormsPage/FormsPage.tsx
+++ src/pages/FormsPage/FormsPage.tsx
@@ -1,28 +1,45 @@
-import {useState} from 'react'
+import { useState } from 'react'
 import s from './FormsPage.module.scss'
-import {ModalForm} from "../../components/FormModal/ui/ModalForm.tsx";
-import {Button, ModalSuccess} from "../../main.ts";
+import { ModalForm } from '../../components/FormModal/ui/ModalForm.tsx'
+import { Button, ModalSuccess } from '../../main.ts'
 
 const FormsPage = () => {
-    const [isFormModalOpen, setIsFormModalOpen] = useState<boolean>(false);
-    const [isSuccessModalOpen, setIsSuccessModalOpen] = useState<boolean>(false);
+	const [isFormModalOpen, setIsFormModalOpen] = useState<boolean>(false)
+	const [isSuccessModalOpen, setIsSuccessModalOpen] = useState<boolean>(false)
 
-    return (
-        <div className={s.root}>
-            <Button as={'button'} onClick={() => setIsFormModalOpen(true)}>ModalForm</Button>
-            <Button as={'button'} onClick={() => setIsSuccessModalOpen(true)}>ModalSucceess</Button>
+	return (
+		<div className={s.root}>
+			<Button
+				as={'button'}
+				onClick={() => setIsFormModalOpen(true)}>
+				ModalForm
+			</Button>
+			<Button
+				as={'button'}
+				onClick={() => setIsSuccessModalOpen(true)}>
+				ModalSucceess
+			</Button>
 
-            <ModalForm submitHandler={(data) => {
-                console.log(data)
-            }} subTitle={'Оставьте номер телефона для получения консультации'} title={'Оставить обращение'}
-                       isRequiredPhoneOrEmail={true}
-                        isRequiredPhone={false} modalWidth={'530px'} isFormOpen={isFormModalOpen}
-                       setIsFormOpen={setIsFormModalOpen} isEmail={true} isTextArea={true}
-                       textAreaPlaceholder={'Напишите ваш вопрос или комментарий'}/>
+			<ModalForm
+				submitHandler={(data) => {
+					console.log(data)
+				}}
+				subTitle={'Оставьте номер телефона для получения консультации'}
+				title={'Оставить обращение'}
+				isRequiredPhoneOrEmail={true}
+				modalWidth={'530px'}
+				isEmail
+				isNews
+				isFormOpen={isFormModalOpen}
+				setIsFormOpen={setIsFormModalOpen}
+			/>
 
-            <ModalSuccess isSuccessOpen={isSuccessModalOpen} setIsSuccess={setIsSuccessModalOpen}/>
-        </div>
-    )
+			<ModalSuccess
+				isSuccessOpen={isSuccessModalOpen}
+				setIsSuccess={setIsSuccessModalOpen}
+			/>
+		</div>
+	)
 }
 
 export default FormsPage
