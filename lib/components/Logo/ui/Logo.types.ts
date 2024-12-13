448206940b327abb28d8d885b41b10642f2a0d74
achernikova <chernikova@realx.tech>
2024-12-13 03:17:08 -0800

    Update version to 0.0.422

--- lib/components/Logo/ui/Logo.types.ts
+++ lib/components/Logo/ui/Logo.types.ts
@@ -1,10 +1,13 @@
 import LogoWhite from '../assets/LogoWhite.svg?url'
 import LogoBlack from '../assets/LogoBlack.svg?url'
 import LogoValue from '../assets/LogoValue.svg?url'
+import LogoDom from '../assets/LogoDom.svg?url'
+import LogoOffice from '../assets/LogoOffice.svg?url'
+
 
 export interface ILogoProps {
 	uk?: boolean
-	variant?: 'black' | 'white' | 'value'
+	variant?: 'black' | 'white' | 'value' | 'dom' | 'office'
 	between?: boolean
 	addClassName?: string
 }
@@ -13,5 +16,7 @@ export const logoVariant = {
 	black: LogoBlack,
 	white: LogoWhite,
 	value: LogoValue,
+	dom: LogoDom,
+	office: LogoOffice
 }
 
