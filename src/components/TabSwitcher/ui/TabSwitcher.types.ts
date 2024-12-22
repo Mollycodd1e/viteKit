2ef99bbb4ed51f85f051863c49dc21770097a248
UzhNaSkovorodke <piratt.kiri@gmail.com>
2024-12-22 16:44:30 +0300

    Update version to 0.0.442

--- src/components/TabSwitcher/ui/TabSwitcher.types.ts
+++ src/components/TabSwitcher/ui/TabSwitcher.types.ts
@@ -1,29 +1,28 @@
 import {ReactNode} from "react";
 
 const TAB_SWITCHER_VARIANTS = {
-	whiteRow: 'whiteRow',
-	grayRow: 'grayRow',
+    whiteRow: 'whiteRow',
+    grayRow: 'grayRow',
 }
 
-type BaseInputProps = JSX.IntrinsicElements['input']
 
-export interface ITabSwitcherItem extends BaseInputProps {
-	addItemClassName?: string
-	index: number
-	isDisabled?: boolean
-	itemsLength: number
-	width?: string
-	width_m?: string
-	width_l?: string
-	children?: ReactNode
+export interface ITabSwitcherItem extends React.InputHTMLAttributes<HTMLInputElement> {
+    addItemClassName?: string
+    index: number
+    isDisabled?: boolean
+    itemsLength: number
+    width?: string
+    width_m?: string
+    width_l?: string
+    children?: ReactNode
 }
 
 export interface TabSwitcherProps {
-	variant?: TABS_VARIANTS
-	addClassName?: string
-	isApart?: boolean
-	col?: boolean
-	children: React.ReactElement[]
+    variant?: TABS_VARIANTS
+    addClassName?: string
+    isApart?: boolean
+    col?: boolean
+    children: React.ReactElement[]
 }
 
 export type TABS_VARIANTS = keyof typeof TAB_SWITCHER_VARIANTS
