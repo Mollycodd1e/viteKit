4fb78f660a588d4a7886501dc97e2807040d0d04
Селиванов Иван <iselivanov@realx.local>
2025-02-12 13:05:58 +0300

    Update version to 0.0.611

--- src/components/FilterWithSave/ui/FilterWithSave.tsx
+++ src/components/FilterWithSave/ui/FilterWithSave.tsx
@@ -205,7 +205,8 @@ export const FilterWithSave = ({
 			<div style={{ position: 'relative' }}>
 				<div
 					className={s.root}
-					onClick={() => {
+					onClick={(e) => {
+						e.stopPropagation()
 						onCLickSelect && onCLickSelect()
 						if (isMobile) {
 							setIsModalOpen((prev) => !prev)
