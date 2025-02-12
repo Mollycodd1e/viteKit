56810a414d9fedff7d609c6affbd6943a48fb43c
Селиванов Иван <iselivanov@realx.local>
2025-02-12 15:05:09 +0300

    Update version to 0.0.618

---
+++ lib/components/FilterWithSave/hooks/useFilterWithSave.ts
@@ -0,0 +1,128 @@
+import { useEffect, useRef, useState } from 'react'
+import { useClientWidth } from '../../../main'
+import { TOption } from '../../Select/ui/Select.types'
+
+interface IuseFilterWithSave {
+	selectOptions?: TOption[]
+	disabledOptions?: TOption[]
+	selectedValues?: TOption[]
+	tabs?: { value: string; state: boolean }[]
+	selectedTabs?: { value: string; state: boolean }[]
+	setSelectedTabs?: React.Dispatch<React.SetStateAction<{ value: string; state: boolean }[]>>
+	onChange?: (selectedOptions: TOption[]) => unknown
+	onClickItem?: (option: TOption | TOption[]) => void
+}
+
+export const useFilterWithSave = ({
+	selectedTabs = [],
+	selectedValues = [],
+	onClickItem,
+	disabledOptions = [],
+	tabs = [],
+	onChange,
+	setSelectedTabs,
+}: IuseFilterWithSave) => {
+	const { isMobile } = useClientWidth()
+	const [isModalOpen, setIsModalOpen] = useState<boolean>(false)
+
+	const containerRef = useRef<HTMLDivElement | null>(null)
+
+	const [initialSelectedTabs, setInitialSelectedTabs] =
+		useState<{ value: string; state: boolean }[]>(selectedTabs)
+	const [initialSelectedOptions] = useState<TOption[]>(selectedValues)
+
+	const [localSelectedTabs, setLocalSelectedTabs] =
+		useState<{ value: string; state: boolean }[]>(initialSelectedTabs)
+	const [selectedOptions, setSelectedOptions] = useState<TOption[]>(initialSelectedOptions)
+
+	const handleOptionClick = (option: TOption) => {
+		onClickItem && onClickItem(option)
+
+		if (disabledOptions.some((disabled) => disabled.value === option.value)) {
+			return
+		}
+		const newSelectedOptions = selectedOptions.some((selected) => selected.value === option.value)
+			? selectedOptions.filter((selected) => selected.value !== option.value)
+			: [...selectedOptions, option]
+
+		setSelectedOptions(newSelectedOptions)
+	}
+
+	const handleClearClick = () => {
+		setInitialSelectedTabs(tabs)
+		setLocalSelectedTabs(tabs)
+		setSelectedOptions([])
+		if (onChange) {
+			onChange([])
+			setSelectedTabs?.(tabs)
+		}
+	}
+
+	const handleResetClick = () => {
+		setSelectedOptions(selectedValues)
+		setLocalSelectedTabs(selectedTabs)
+	}
+
+	const handleCloseModal = () => {
+		handleResetClick()
+		setIsModalOpen(false)
+	}
+
+	const handleSaveClick: React.MouseEventHandler<HTMLButtonElement> = (event) => {
+		event.stopPropagation()
+		if (onChange) {
+			setSelectedTabs?.(localSelectedTabs)
+			onChange(selectedOptions)
+		}
+		setIsModalOpen(false)
+	}
+
+	useEffect(() => {
+		if (!isModalOpen) {
+			setLocalSelectedTabs((prevTabs) => {
+				if (JSON.stringify(prevTabs) !== JSON.stringify(selectedTabs)) {
+					return selectedTabs
+				}
+				return prevTabs
+			})
+		}
+	}, [isModalOpen, selectedTabs])
+
+	useEffect(() => {
+		const handleDocumentClick = (event: MouseEvent) => {
+			if (!containerRef.current) return
+
+			if (containerRef.current.contains(event.target as Node)) {
+				return
+			}
+
+			handleCloseModal()
+		}
+
+		if (isModalOpen) {
+			document.addEventListener('click', handleDocumentClick)
+		} else {
+			document.removeEventListener('click', handleDocumentClick)
+		}
+
+		return () => {
+			document.removeEventListener('click', handleDocumentClick)
+		}
+	}, [isModalOpen])
+
+	return {
+		isMobile,
+		handleClearClick,
+		handleCloseModal,
+		handleOptionClick,
+		handleSaveClick,
+		localSelectedTabs,
+		selectedOptions,
+		setSelectedOptions,
+		isModalOpen,
+		setIsModalOpen,
+		setLocalSelectedTabs,
+		containerRef,
+	}
+}
+
--- src/components/FilterWithSave/ui/FilterWithSave.tsx
+++ src/components/FilterWithSave/ui/FilterWithSave.tsx
@@ -1,4 +1,3 @@
-import { useEffect, useRef, useState } from 'react'
 import { NewIcon } from '../../NewIcon'
 import s from './s.module.scss'
 import { Modal } from '../../Modal'
@@ -7,7 +6,7 @@ import { Option } from '../../Select/ui/Option'
 import { TOption } from '../../Select/ui/Select.types'
 import { Button } from '../../Button'
 import classNames from 'classnames/bind'
-import { useClientWidth } from '../../../main'
+import { useFilterWithSave } from '../hooks/useFilterWithSave'
 
 const cx = classNames.bind(s)
 
@@ -25,104 +24,38 @@ interface IFilterWithSave {
 }
 
 export const FilterWithSave = ({
-	disabledOptions = [],
+	disabledOptions,
 	tabs = [],
 	selectOptions = [],
 	onChange,
-	selectedValues = [],
+	selectedValues,
 	setSelectedTabs,
-	selectedTabs = [],
+	selectedTabs,
 	onClickItem,
 	onCLickSelect,
 	btnName = 'Фильтр',
 }: IFilterWithSave) => {
-	const { isMobile } = useClientWidth()
-	const [isModalOpen, setIsModalOpen] = useState<boolean>(false)
-
-	const containerRef = useRef<HTMLDivElement | null>(null)
-
-	const [initialSelectedTabs, setInitialSelectedTabs] =
-		useState<{ value: string; state: boolean }[]>(selectedTabs)
-	const [initialSelectedOptions] = useState<TOption[]>(selectedValues)
-
-	const [localSelectedTabs, setLocalSelectedTabs] =
-		useState<{ value: string; state: boolean }[]>(initialSelectedTabs)
-	const [selectedOptions, setSelectedOptions] = useState<TOption[]>(initialSelectedOptions)
-
-	const handleOptionClick = (option: TOption) => {
-		onClickItem && onClickItem(option)
-
-		if (disabledOptions.some((disabled) => disabled.value === option.value)) {
-			return
-		}
-		const newSelectedOptions = selectedOptions.some((selected) => selected.value === option.value)
-			? selectedOptions.filter((selected) => selected.value !== option.value)
-			: [...selectedOptions, option]
-
-		setSelectedOptions(newSelectedOptions)
-	}
-
-	const handleClearClick = () => {
-		setInitialSelectedTabs(tabs)
-		setLocalSelectedTabs(tabs)
-		setSelectedOptions([])
-		if (onChange) {
-			onChange([])
-			setSelectedTabs?.(tabs)
-		}
-	}
-
-	const handleResetClick = () => {
-		setSelectedOptions(selectedValues)
-		setLocalSelectedTabs(selectedTabs)
-	}
-
-	const handleCloseModal = () => {
-		handleResetClick()
-		setIsModalOpen(false)
-	}
-
-	const handleSaveClick: React.MouseEventHandler<HTMLButtonElement> = (event) => {
-		event.stopPropagation()
-		if (onChange) {
-			setSelectedTabs?.(localSelectedTabs)
-			onChange(selectedOptions)
-		}
-		setIsModalOpen(false)
-	}
-
-	useEffect(() => {
-		if (!isModalOpen) {
-			setLocalSelectedTabs((prevTabs) => {
-				if (JSON.stringify(prevTabs) !== JSON.stringify(selectedTabs)) {
-					return selectedTabs
-				}
-				return prevTabs
-			})
-		}
-	}, [isModalOpen, selectedTabs])
-
-	useEffect(() => {
-		const handleDocumentClick = (event: MouseEvent) => {
-			if (!containerRef.current) return
-
-			if (containerRef.current.contains(event.target as Node)) {
-				return
-			}
-			console.log('я тут')
-			handleCloseModal()
-		}
-
-		if (isModalOpen) {
-			document.addEventListener('click', handleDocumentClick)
-		} else {
-			document.removeEventListener('click', handleDocumentClick)
-		}
-
-		return () => {
-			document.removeEventListener('click', handleDocumentClick)
-		}
-	}, [isModalOpen])
+	const {
+		handleCloseModal,
+		localSelectedTabs,
+		selectedOptions,
+		isMobile,
+		handleOptionClick,
+		handleClearClick,
+		handleSaveClick,
+		isModalOpen,
+		setLocalSelectedTabs,
+		setIsModalOpen,
+		containerRef,
+	} = useFilterWithSave({
+		selectedTabs,
+		onClickItem,
+		setSelectedTabs,
+		tabs,
+		onChange,
+		disabledOptions,
+		selectedValues,
+	})
 
 	const ModalBody = () => {
 		return (
@@ -225,7 +158,77 @@ export const FilterWithSave = ({
 					<div
 						ref={containerRef}
 						className={cx(s.desktopWrapper, { [s.desktopWrapperOpen]: isModalOpen })}>
-						<ModalBody />
+						<div
+							className={s.closeBtn}
+							onClick={() => {
+								handleCloseModal()
+							}}>
+							<NewIcon
+								name={'close'}
+								size='24'
+							/>
+						</div>
+
+						<div className={s.modalTitle}>Фильтры</div>
+						<div className={s.tabSWitcherWrapper}>
+							<div className={s.switcherTitle}>Готовность</div>
+							<div className={s.switcherScroll}>
+								<TabSwitcher
+									variant={'grayRow'}
+									addClassName={s.tabs}
+									isApart={true}>
+									{tabs?.map((m, i) => {
+										const value = m.value
+										return (
+											<TabSwitcher.Item
+												itemsLength={tabs?.length}
+												key={i}
+												mini
+												value={value}
+												onClick={() => {}}
+												checked={localSelectedTabs?.find((e) => e.value === value)?.state ?? false}
+												onChange={() => {
+													setLocalSelectedTabs?.((prev: { value: string; state: boolean }[]) =>
+														prev.map((d) => (d.value === value ? { ...d, state: !d.state } : d))
+													)
+												}}
+												index={i}
+												addItemClassName={s.tabSwitcher}>
+												{value}
+											</TabSwitcher.Item>
+										)
+									})}
+								</TabSwitcher>
+							</div>
+						</div>
+						<div className={s.selectWrapper}>
+							{selectOptions?.map((option, i) => (
+								<Option
+									key={i}
+									addClassName={s.filterOption}
+									disabledOptions={[]}
+									clickableOptions={[]}
+									selectedOptions={selectedOptions}
+									option={option}
+									handleOptionClick={handleOptionClick}
+								/>
+							))}
+						</div>
+						<div className={s.btnWrapper}>
+							<Button
+								as='button'
+								variant='gray'
+								additionalClass={s.resetBtn}
+								onClick={handleClearClick}>
+								Сбросить
+							</Button>
+							<Button
+								as='button'
+								additionalClass={s.saveBtn}
+								onClick={handleSaveClick}>
+								Сохранить
+							</Button>
+						</div>
 					</div>
 				)}
 			</div>
