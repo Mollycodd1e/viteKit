4e15a87ab51ccf314973f888b7782b052d42a1b3
Селиванов Иван <iselivanov@realx.local>
2025-02-17 14:10:08 +0300

    Update version to 0.0.660

--- src/components/LotCard/ui/LotCard.tsx
+++ src/components/LotCard/ui/LotCard.tsx
@@ -179,6 +179,7 @@ export const LotCard = ({
 						additionalClass={s.fullscreenBtn}
 						onClick={(e) => {
 							e.preventDefault()
+							e.stopPropagation()
 							handleFullScreenBtn && handleFullScreenBtn()
 						}}>
 						<NewIcon
