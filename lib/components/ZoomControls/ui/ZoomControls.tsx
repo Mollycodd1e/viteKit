a569b0667517df19178eb8b0589b77753aa811bb
UzhNaSkovorodke <tukalov.kirill99@gmail.com>
2025-04-06 20:12:42 +0300

    Update version to 0.0.714

--- lib/components/ZoomControls/ui/ZoomControls.tsx
+++ lib/components/ZoomControls/ui/ZoomControls.tsx
@@ -1,23 +1,21 @@
-import { useClientWidth } from '../../../main'
 import { RoundButton } from '../../RoundButton'
 import s from './ZoomControls.module.scss'
 import { IZoomControls } from './ZoomControls.types'
 
 export const ZoomControls = ({ size = 'medium', zoomIn, zoomOut }: IZoomControls) => {
-	const { isDesktop } = useClientWidth()
 
 	return (
 		<div className={s.root}>
 			<RoundButton
 				iconName='plus'
-				size={isDesktop ? 'large' : size}
+				size={size}
 				color='#141416'
 				additionalClass={s.plusBtn}
 				onClick={zoomIn}
 			/>
 			<RoundButton
 				iconName='minus'
-				size={isDesktop ? 'large' : size}
+				size={size}
 				color='#141416'
 				additionalClass={s.minusBtn}
 				onClick={zoomOut}
