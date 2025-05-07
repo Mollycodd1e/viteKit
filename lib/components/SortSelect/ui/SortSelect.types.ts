8f6a3cd614afdfc14f4e881eefc89fb0e51f1ff8
Mollycodd1e <selik7@mail.ru>
2025-05-07 12:07:00 +0300

    Update version to 0.0.769

--- lib/components/SortSelect/ui/SortSelect.types.ts
+++ lib/components/SortSelect/ui/SortSelect.types.ts
@@ -1,4 +1,4 @@
-import {TOption} from "../../Select/ui/Select.types.ts";
+import { TOption } from '../../Select/ui/Select.types.ts'
 
 export interface Option {
 	value: string | number
@@ -16,7 +16,7 @@ export interface SortSelectProps {
 	additionalClass?: string
 	mini?: boolean
 	openOnTop?: boolean
-
+	onOpenClick?: () => void
 	onClickItem?: (option: TOption | TOption[]) => void
 	onCLickSelect?: () => void
 }
