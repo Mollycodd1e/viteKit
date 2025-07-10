51347a00a9693c03c79b21e50b85c0d1ebd649fd
Natalia <35937974+Nataly4444@users.noreply.github.com>
2025-07-10 12:27:46 +0300

    Update version to 0.0.820

--- lib/components/Logo/ui/Logo.types.ts
+++ lib/components/Logo/ui/Logo.types.ts
@@ -1,7 +1,25 @@
+import LogoWhite from '../assets/LogoWhite.svg?url'
+import LogoBlack from '../assets/LogoBlack.svg?url'
+import LogoValue from '../assets/LogoValue.svg?url'
+import LogoDom from '../assets/LogoDom.svg?url'
+import LogoOffice from '../assets/LogoOffice.svg?url'
+import LogoNewOffice from '../assets/LogoNewOffice.svg?url'
+import LogoNewDom from '../assets/LogoNewDom.svg?url'
+
 export interface ILogoProps {
 	uk?: boolean
 	variant?: 'black' | 'white' | 'value' | 'dom' | 'office' | 'newOffice' | 'newDom'
 	between?: boolean
 	addClassName?: string
 	onClick?: () => void
+}
+
+export const logoVariant = {
+	black: LogoBlack,
+	white: LogoWhite,
+	value: LogoValue,
+	dom: LogoDom,
+	office: LogoOffice,
+	newOffice: LogoNewOffice,
+	newDom: LogoNewDom,
 }
--- src/components/Logo/ui/Logo.tsx
+++ src/components/Logo/ui/Logo.tsx
@@ -1,9 +1,7 @@
 import { Text } from '../../Text'
 import s from './Logo.module.scss'
 import classNames from 'classnames'
-import { ILogoProps } from './Logo.types'
-import logoTypes from './logoTypes'
-import { Suspense } from 'react'
+import { ILogoProps, logoVariant } from './Logo.types'
 
 export const Logo = ({
 	uk = false,
@@ -13,7 +11,6 @@ export const Logo = ({
 	onClick,
 }: ILogoProps) => {
 	const cx = classNames.bind(s)
-	const LogoComponent = logoTypes[variant]
 
 	return (
 		<div
@@ -21,18 +18,18 @@ export const Logo = ({
 			onClick={() => {
 				onClick && onClick()
 			}}>
-			<Suspense fallback={<div>Загрузка...</div>}>
-				{LogoComponent ? (
-					<LogoComponent className={cx(
-						s.logo,
-						{ [s.logoValue]: variant === 'value' },
-						{ [s.logoDom]: variant === 'dom' },
-						{ [s.logoOffice]: variant === 'office' },
-						{ [s.logoNewOffice]: variant === 'newOffice' },
-						{ [s.logoNewDom]: variant === 'newDom' }
-					)} />
-				) : null}
-			</Suspense>
+				<img
+				src={logoVariant[variant]}
+				alt='Лого'
+				className={cx(
+					s.logo,
+					{ [s.logoValue]: variant === 'value' },
+					{ [s.logoDom]: variant === 'dom' },
+					{ [s.logoOffice]: variant === 'office' },
+					{ [s.logoNewOffice]: variant === 'newOffice' },
+					{ [s.logoNewDom]: variant === 'newDom' }
+				)}
+			/>
 			{uk && (
 				<Text
 					className={cx(s.uk, s[`uk_${variant}`])}
