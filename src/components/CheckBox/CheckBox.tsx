c534e9405fcca316269a0749e72386f66ba4e70f
Mollycodd1e <selik7@mail.ru>
2025-08-12 21:15:54 +0300

    Update version to 0.0.845

--- src/components/CheckBox/CheckBox.tsx
+++ src/components/CheckBox/CheckBox.tsx
@@ -1,41 +1,47 @@
-import {Input} from "../Input";
-import classNames from "classnames";
-import {forwardRef} from "react";
+import { Input } from '../Input'
+import classNames from 'classnames'
+import { forwardRef } from 'react'
 
 import s from './s.module.scss'
-import {NewIcon} from "../NewIcon";
+import { NewIcon } from '../NewIcon'
 
 const cx = classNames.bind(s)
 
 interface Props {
-    text?: string;
-    error: boolean
-    isChecked?: boolean
-    children?: React.ReactNode
+	text?: string
+	error: boolean
+	isChecked?: boolean
+	children?: React.ReactNode
 }
 
-export type Ref = HTMLInputElement;
+export type Ref = HTMLInputElement
 
+export const CheckBox = forwardRef<Ref, Props>(
+	({ text, isChecked, error, children, ...props }, ref) => {
+		return (
+			<label className={cx(s.checkboxLabel, { [s.error]: error })}>
+				<Input
+					type='checkbox'
+					{...props}
+					ref={ref}
+				/>
 
-export const CheckBox = forwardRef<Ref, Props>(({
-                                                    text,
-                                                    isChecked,
-                                                    error,
-                                                    children,
-                                                    ...props
-                                                },
-                                                ref) => {
-    return (
-        <label className={cx(s.checkboxLabel, {[s.error]: error})}>
-            <Input type="checkbox" {...props} ref={ref}/>
+				{isChecked && (
+					<NewIcon
+						additionalClass={s.checkIcon}
+						color={'#B1B5C3'}
+						strokeWidth={'1.5'}
+						name={'check'}
+						size='20'
+					/>
+				)}
 
-            {isChecked && <NewIcon additionalClass={s.checkIcon}
-                                   color={'white'} strokeWidth={'3'}
-                                   name={'check'}
-                                   size='16'/>}
-
-            <div className={cx(s.labelText, {[s.error]: error})}
-                 dangerouslySetInnerHTML={text ? {__html: text} : undefined}
-            >{children}</div>
-        </label>)
-})
+				<div
+					className={cx(s.labelText, { [s.error]: error })}
+					dangerouslySetInnerHTML={text ? { __html: text } : undefined}>
+					{children}
+				</div>
+			</label>
+		)
+	}
+)
--- src/pages/CheckboxPage/CheckboxPage.tsx
+++ src/pages/CheckboxPage/CheckboxPage.tsx
@@ -3,20 +3,34 @@ import { CheckBoxOld } from '../../components/CheckBoxOld/CheckBoxOld'
 import s from './CheckboxPage.module.scss'
 
 export const CheckboxPage = () => {
-
-    // доделаю вместе с рефактором checkbox
+	// доделаю вместе с рефактором checkbox
 
 	return (
 		<div className={s.root}>
 			<div>
-            <CheckBoxOld flex isChecked={true} emitIsChecked={() => console.log('emitIsChecked')} isBlackCheck>
-                <div>CheckBoxOld isBlackCheck flex</div>
-            </CheckBoxOld>
-            <CheckBoxOld isChecked={true} emitIsChecked={() => console.log('emitIsChecked')} isLightCheck>
-                <div>CheckBoxOld isLightCheck</div>
-            </CheckBoxOld>
-			<CheckBox isChecked={true} error><div>CheckBox error</div></CheckBox>
-            <CheckBox isChecked={false} error={false}><div>CheckBox</div></CheckBox>
+				<CheckBoxOld
+					flex
+					isChecked={true}
+					emitIsChecked={() => console.log('emitIsChecked')}
+					isBlackCheck>
+					<div>CheckBoxOld isBlackCheck flex</div>
+				</CheckBoxOld>
+				<CheckBoxOld
+					isChecked={true}
+					emitIsChecked={() => console.log('emitIsChecked')}
+					isLightCheck>
+					<div>CheckBoxOld isLightCheck</div>
+				</CheckBoxOld>
+				<CheckBox
+					isChecked={true}
+					error>
+					<div>CheckBox error</div>
+				</CheckBox>
+				<CheckBox
+					isChecked={false}
+					error={false}>
+					<div>CheckBox</div>
+				</CheckBox>
 			</div>
 		</div>
 	)
